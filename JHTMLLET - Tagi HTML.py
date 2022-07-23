e90b11ae0f24c8b6448e9be17141e265cd2eaf5a
cynarski <93757027+cynarski@users.noreply.github.com>
2022-07-23 22:14:49 +0200

    Create  JHTMLLET - Tagi HTML.py

---
+++ JHTMLLET - Tagi HTML.py
@@ -0,0 +1,18 @@
+import sys
+def tagi_HTML(word: str):
+    tag = False
+    to_fix = list(map(str,word))
+    for i in range(0, len(to_fix)):
+        if(to_fix[i] == '<'):
+            tag = True 
+        elif(to_fix[i] == '>'):
+            tag = False
+        if(tag):
+            to_fix[i] = str(to_fix[i]).upper()
+    result = ''.join(to_fix)
+    return result
+
+
+
+for line in sys.stdin:
+    print(tagi_HTML(line))
