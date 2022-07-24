04d3bdaad59c94e7e137601c0dee469e73b2460e
cynarski <93757027+cynarski@users.noreply.github.com>
2022-07-24 15:10:27 +0200

    Create TRN - Transponowanie macierzy.py

---
+++ TRN - Transponowanie macierzy.py
@@ -0,0 +1,17 @@
+def matrix(row: int,col: int):
+    result = []
+    for i in range(row):
+        result.append(list(map(int,input().split())))
+    for i in range(col):
+        for j in range(row):
+            print(int(result[j][i]), end= ' ')
+
+        print()
+
+
+
+dane = input().split(' ')
+row = int(dane[0])
+col = int(dane[1])
+matrix(row,col)
+
