c061451e11a6b43a2dc367fc91612211f7c89159
cynarski <93757027+cynarski@users.noreply.github.com>
2022-07-26 13:40:07 +0200

    Create  PP0601A2 - Test 3.py

---
+++ PP0601A2 - Test 3.py
@@ -0,0 +1,12 @@
+first = True
+past = 0
+counter = 0
+while True:
+    current = int(input())
+    print(current)
+    if current == 42 and past != 42 and not first:
+        counter +=1
+    if counter == 3:
+        break
+    past = current
+    first = False
